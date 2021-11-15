7c26c2352c02662d61bb2d01569b959bcb46d5fe
이승환 <tmdghks7836@naver.com>
2021-11-15 18:23:05 +0900

    configure source

---
+++ src/main/java/com/jwt/radis/filter/JwtTokenFilter.java
@@ -0,0 +1,72 @@
+package com.jwt.radis.filter;
+
+import com.jwt.radis.model.dto.UserDetailsImpl;
+import com.jwt.radis.utils.JwtTokenUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import rc.meeting.server.api.model.MeetingUser;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+import static java.util.Optional.ofNullable;
+import static org.springframework.util.StringUtils.isEmpty;
+
+@Component
+@Slf4j
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtTokenUtils jwtTokenUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        // Get authorization header and validate
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        log.info("jwt ============> {}", header);
+
+        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // Get jwt token and validate
+        final String token = header.split(" ")[1].trim();
+        if (!jwtTokenUtils.validate(token)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // Get user identity and set it on the spring security context
+        UserDetailsImpl userDetails = new UserDetailsImpl(jwtTokenUtils.getUserId(token), "박영일");
+
+        Integer uid = jwtTokenUtils.getUid(token);
+        if (uid != null) {
+            userDetails.setId(new Long(uid));
+        }
+        userDetails.setAuthorities(AuthorityUtils.createAuthorityList(jwtTokenUtils.getRole(token)));
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                ofNullable(userDetails).map(UserDetails::getAuthorities).orElse(Collections.emptyList())
+        );
+        authentication.setDetails(userDetails);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+}
